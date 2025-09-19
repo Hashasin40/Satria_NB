5316ed258388987f41bc8f3c3e87efd4011b1d5f
Satria Nurrahmat Baihaqi <rahmatsatria088@gmail.com>
2025-09-19 09:51:12 +0700

    Rename navbar.jsx to Navbar.jsx with clean case

--- src/component/DashboardLayout.jsx
+++ src/component/DashboardLayout.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Background from '../assets/background.jpg';
-import Navbar from "./navbar";
-import Footer from "./footer";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 import CursorTrail from "./CursorTrail";
 
 const DashboardLayout = () => {
